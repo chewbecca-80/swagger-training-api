1c90707f680d7c7a33afeb4bb7c6c9c10fb53f40
Rebecca Servaites <kathryn.servaites@udri.udayton.edu>
2020-11-09 18:08:44 -0500

    feature: initial commit

---
+++ src/servaites/python_demo_api/server.py
@@ -0,0 +1,25 @@
+import importlib.resources
+
+import connexion
+import ruamel.yaml
+
+
+YAML = ruamel.yaml.YAML(typ='safe')
+with importlib.resources.path('servaites.python_demo_api', 'api.yaml') as f:
+    openapi_dict = YAML.load(f)
+
+def get_health():
+    return {'message': 'Everything is A-OK'}
+
+def get_echo(message: str):
+    data = {'echo': f'{message} {message} {message}'}
+    return data
+
+def main(port: int = 9090):
+    app = connexion.FlaskApp(__name__, port=9090)
+    app.add_api(openapi_dict, arguments={'title': 'Echo Chamber'})
+    app.run()
+
+if __name__ == '__main__':
+    main()
+
