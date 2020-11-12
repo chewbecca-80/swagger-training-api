031e52722f2818b7d7bd150c7fbf535b3fd04364
Rebecca Servaites <kathryn.servaites@udri.udayton.edu>
2020-11-12 14:37:40 -0500

    fix: add brackets

--- test/test_simple.py
+++ test/test_simple.py
@@ -5,5 +5,5 @@ from servaites.python_demo_api import echo
 
 def test_make_echo():
     message = echo.make_echo("hello")
-    data = json.load('message': 'hello hello hello')
+    data = json.load({'message': 'hello hello hello'})
     assert data = message
