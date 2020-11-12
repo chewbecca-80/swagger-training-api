2b11bff814eb15bafd7a795d38198667e25b40c3
Rebecca Servaites <kathryn.servaites@udri.udayton.edu>
2020-11-12 14:41:04 -0500

    fix: equals sign

--- test/test_simple.py
+++ test/test_simple.py
@@ -6,4 +6,4 @@ from servaites.python_demo_api import echo
 def test_make_echo():
     message = echo.make_echo("hello")
     data = json.load({'message': 'hello hello hello'})
-    assert data = message
+    assert data == message
