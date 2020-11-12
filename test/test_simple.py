4aff4799e77399b2cc5cefe11f508dc89f0bbddc
Rebecca Servaites <kathryn.servaites@udri.udayton.edu>
2020-11-12 14:32:59 -0500

    fix: update unit test

--- test/test_simple.py
+++ test/test_simple.py
@@ -1,5 +1,9 @@
+import json
+
 from servaites.python_demo_api import echo
+
 
 def test_make_echo():
     message = echo.make_echo("hello")
-    assert "hello hello hello" = message
+    data = json.load('message': 'hello hello hello')
+    assert data = message
