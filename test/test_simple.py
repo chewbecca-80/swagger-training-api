e718fc48f5e58541102516e61592c591ce15e583
Rebecca Servaites <kathryn.servaites@udri.udayton.edu>
2020-11-16 17:05:36 -0500

    fix: unit test, not func

--- test/test_simple.py
+++ test/test_simple.py
@@ -1,9 +1,7 @@
-import json
-
 from servaites.python_demo_api import echo
 
 
 def test_make_echo():
     message = echo.make_echo("hello")
-    data = json.load({'message': 'hello hello hello'})
-    assert data == message
+    test_message = "hello hello hello"
+    assert test_message == message
